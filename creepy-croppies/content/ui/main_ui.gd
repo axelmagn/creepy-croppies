9de61cbd0753e89640ca19b828e22dc6845e0830
Benjamin Dums <benjamin.dums@gmail.com>
2025-10-29 15:59:08 +0100

    add rudimentary day transition/summary system

--- creepy-croppies/content/ui/main_ui.gd
+++ creepy-croppies/content/ui/main_ui.gd
@@ -2,7 +2,9 @@ class_name MainUI extends CanvasLayer
 
 @export var main_menu: MainMenu
 @export var player_hud: PlayerHUD
+@export var day_summary: DaySummary
 
 func _ready() -> void:
 	assert(main_menu)
 	assert(player_hud)
+	assert(day_summary)

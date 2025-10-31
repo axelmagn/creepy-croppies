182f0cb2db14d1fe5a95b882667e1c26a881e184
Benjamin Dums <benjamin.dums@gmail.com>
2025-10-31 23:16:33 +0100

    fix interaction bug with bridge

--- creepy-croppies/content/bridge/bridge.gd
+++ creepy-croppies/content/bridge/bridge.gd
@@ -4,6 +4,7 @@ class_name Bridge extends Node
 @export var broken_bridge_collider: CollisionShape2D
 @export var fixed_bridge: Node2D
 @export var fix_cost: int = 100
+@export var interactionArea: Area2D
 
 var is_fixed := false
 
@@ -11,6 +12,7 @@ func _ready() -> void:
 	assert(broken_bridge)
 	assert(fixed_bridge)
 	assert(broken_bridge_collider)
+	assert(interactionArea)
 	broken_bridge.show()
 	fixed_bridge.hide()
 	broken_bridge_collider.disabled = false
@@ -25,3 +27,4 @@ func fix() -> void:
 	broken_bridge.hide()
 	broken_bridge_collider.disabled = true
 	fixed_bridge.show()
+	interactionArea.queue_free()
